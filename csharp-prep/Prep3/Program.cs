5953058dc5f29e286d9d558a442399ac163f6590
nelszack <nel20012@byui.edu>
2024-04-24 11:42:42 -0600

    finished prep 3 assignment with streach chalenges

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,30 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        string newGame="yes";
+        do{
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        Console.WriteLine($"{magicNumber}");
+        int number=-9;
+        int count=0;
+            do {
+            Console.Write("what is your guess? ");
+            string guess=Console.ReadLine();
+            number=int.Parse(guess);
+            if (number>magicNumber){
+                Console.WriteLine("Lower");
+            }
+            else if (number<magicNumber){
+                Console.WriteLine("Higher");
+            }
+            count+=1;
+        }while (number!=magicNumber);
+        Console.WriteLine($"correct number of trys {count}");
+        Console.Write("do you want to play again? ");
+        newGame=Console.ReadLine();
+        }while(newGame=="yes");
+
     }
 }

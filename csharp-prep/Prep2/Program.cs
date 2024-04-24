88ff8a23b19a6b88bf7a3ddb75ff6795735aacfd
nelszack <nel20012@byui.edu>
2024-04-23 21:35:28 -0600

    finished prep 2 and did the streach chalenges

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage ");
+        string percentage=Console.ReadLine();
+        float grade=float.Parse(percentage);
+        string letter= "";
+        if (grade>=90){
+            if (grade>90 && grade<94){
+                letter="A-";
+            }
+            else{
+                letter="A";
+            }
+        }
+        else if (grade>=80){
+            if (grade>80 && grade<84){
+                letter="B-";
+            }
+            else if (grade>87 && grade<90){
+                letter="B+";
+            }
+            else{
+                letter="B";
+            }
+        }
+        else if (grade>=70){
+           if (grade>70 && grade<74){
+                letter="C-";
+            }
+            else if (grade>77 && grade<80){
+                letter="C+";
+            }
+            else{
+                letter="C";
+            }
+        }
+        else if (grade>=60){
+            if (grade>60 && grade<64){
+                letter="D-";
+            }
+            else if (grade>67 && grade<70){
+                letter="D+";
+            }
+            else{
+                letter="D";
+            }
+        }
+        else if (grade<60){
+            letter="F";
+        }
+        if (grade>=70){
+            Console.Write($"your grade is a {letter} you passed!");
+        }
+        else if (grade<70){
+            Console.Write($"your grade is a {letter} you did not pass try again");
+        }
     }
 }

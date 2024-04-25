df07734935bfd0c8f228f26f6d5b6046d25a2ea1
nelszack <nel20012@byui.edu>
2024-04-25 11:21:02 -0600

    finished the prep 4 and streach chalenges

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,7 +7,7 @@ class Program
         List<int> numbListzn= new List<int>();
         do
         {
-            Console.Write("enter a numeber");
+            Console.Write("enter a numeber ");
             string numbstrzn=Console.ReadLine();
             numbzn=int.Parse(numbstrzn);
             if (numbzn!=0){
@@ -16,12 +16,29 @@ class Program
         } while (numbzn!=0);
         int lennumblistzn=numbListzn.Count();
         Console.WriteLine(lennumblistzn);
-        for (int i=0;i<lennumblistzn;i++){
-            Console.Write(i);
-            
+        float totalzn=0;
+        int highestzn=0;
+        int smallPositivezn=100000000;
+        numbListzn.Sort();
+        foreach (int i in numbListzn){
+            totalzn+=i;
+            if (i>highestzn){
+                highestzn=i;
+            }
+            if (i<smallPositivezn&&i>0)
+            {
+                smallPositivezn=i;
+            }
+        }
+        Console.WriteLine($"the sum of the list is {totalzn}");
+        Console.WriteLine($"the avg of the list is {totalzn/(lennumblistzn)}");
+        Console.WriteLine($"the largest number in the list is {highestzn}");
+        Console.WriteLine($"the smallest positive number in the list is {smallPositivezn}");
+        Console.WriteLine("Sorted list");
+        foreach (int numb in numbListzn){
+            Console.WriteLine(numb);
         }
 
         }
         
     }
-}

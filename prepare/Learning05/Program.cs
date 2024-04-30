dfb1e8c8f9365be921c06ce51fc770eedfefe136
nelszack <nel20012@byui.edu>
2024-04-30 11:06:31 -0600

    finished w09 prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes= new List<Shape>();
+        Circle c1=new Circle("red",3);
+        shapes.Add(c1);
+        Square s1=new Square("blue",6);
+        shapes.Add(s1);
+        Rectangle r1=new Rectangle("green",3,4);
+        shapes.Add(r1);
+        for(int i=0;i<3;i++){
+            string color=shapes[i].getColor();
+            double area=shapes[i].GetArea();
+            Console.WriteLine($"color: {color} area: {area}");
+            {
+                
+            }
+        }
     }
 }

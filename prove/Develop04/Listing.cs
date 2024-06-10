27ac90f2c2527ed1da4fed930f6200d4eeb26b9a
nelszack <nel20012@byui.edu>
2024-06-10 12:23:55 -0600

    added to the list on listing class

--- prove/Develop04/Listing.cs
+++ prove/Develop04/Listing.cs
@@ -1,6 +1,6 @@
 using System;
 public class Listing:Base{
-    private List<string> _prompt=["this is fun"];
+    private List<string> _prompt=["Who are people that you appreciate?","What are personal strengths of yours?","Who are people that you have helped this week?","When have you felt the Holy Ghost this month?","Who are some of your personal heroes?"];
     private List<string> _responce=[];
 
     public Listing(string message): base(message){

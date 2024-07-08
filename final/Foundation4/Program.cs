d39e507b0d0097c0257b9617032d79cde921eb00
nelszack <nel20012@byui.edu>
2024-07-08 11:58:43 -0600

    made it so that the date is pulled from system

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Activity activity=new();
-        Running running1 = new("Nov 03 2022",30,3.0);
-        Biking biking1= new("Nov 03 2022",30,6.0);
-        Swimming swimming1 =new("Nov 03 2022",30,3000.0/31);
+        Running running1 = new(DateTime.Now.ToString("MMMM dd yyyy"),30,3.0);
+        Biking biking1= new(DateTime.Now.ToString("MMMM dd yyyy"),30,6.0);
+        Swimming swimming1 =new(DateTime.Now.ToString("MMMM dd yyyy"),30,3000.0/31);
         activity.AddList(running1);
         activity.AddList(biking1);
         activity.AddList(swimming1);
